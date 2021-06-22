1fb27f1179797ee02c23e4d36d15542431d6be98
Jorge Loredo <81264704+MrRedPanda69@users.noreply.github.com>
2021-06-21 19:13:36 -0500

    First Commit

---
+++ src/components/Frase.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ContFrase = styled.div`
+    padding: 3rem;
+    margin-top: 5rem;
+    border-radius: 0.5rem;
+    background-color: #ffffff;
+    max-width: 800px;
+
+    @media (min-width: 992px) {
+        margin-top: 5rem;
+    }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: #000;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
+`;
+
+const Frase = ({frase}) => {
+
+    return (
+        <ContFrase>
+            <h1>{frase.quote}</h1>
+            <p>-{frase.author}</p>
+        </ContFrase>
+    );
+}
+
+export default Frase;
